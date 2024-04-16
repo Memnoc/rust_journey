46a868e4de9e22480860db4996330229b0adb7d3
Memnoc <smartdroidesign@gmail.com>
2024-04-16 06:35:48 +0100

    notes on the code

--- the_book/guessing_game/src/main.rs
+++ the_book/guessing_game/src/main.rs
@@ -1,26 +1,29 @@
+use rand::Rng;
 use std::{cmp::Ordering, io};
-
-use rand::Rng; // prelude
 
 fn main() {
     println!("== Guessing Game ==");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100); // generating a random number from 1
-                                                               // to 100
+    //NOTE: generating a random num from 1 to 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Secret number: {}", secret_number);
 
     println!("== Please input your guess ==");
+    let mut guess = String::new();
 
-    let mut guess = String::new(); // new string
-
+    //NOTE: intake user input while handling failures
     io::stdin()
-        .read_line(&mut guess) // intake user input
-        .expect("Failed to read line"); // handling failures
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
+    //NOTE: converting guess string to num with parse()
+    // parse also returns Result type so we can do error handling
+    // this is also demonstrating "shadowing"
+    // notice that we need to specify the number type
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
     println!("You guessed: {guess}");
 
+    //NOTE: comparing the guess and the number
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big"),
