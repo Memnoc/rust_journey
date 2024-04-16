b4e1960e9df54377cf2fe90b05272771052d3634
Memnoc <smartdroidesign@gmail.com>
2024-04-16 07:05:46 +0100

    varibles - constants

--- the_book/variables/src/main.rs
+++ the_book/variables/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    // NOTE: with constants, you need all caps name and declare the type
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
 }
