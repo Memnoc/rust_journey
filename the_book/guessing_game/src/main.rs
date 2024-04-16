b68c0335f6071d5649fa279448ba4e5a58e082eb
Memnoc <smartdroidesign@gmail.com>
2024-04-16 06:50:38 +0100

    final code and more comments

--- the_book/guessing_game/src/main.rs
+++ the_book/guessing_game/src/main.rs
@@ -6,27 +6,38 @@ fn main() {
 
     //NOTE: generating a random num from 1 to 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number: {}", secret_number);
-
-    println!("== Please input your guess ==");
-    let mut guess = String::new();
-
-    //NOTE: intake user input while handling failures
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    //NOTE: converting guess string to num with parse()
-    // parse also returns Result type so we can do error handling
-    // this is also demonstrating "shadowing"
-    // notice that we need to specify the number type
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
 
     //NOTE: comparing the guess and the number
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+    // adding a loop to give users more chances to guess
+    loop {
+        println!("== Please input your guess ==");
+        let mut guess = String::new();
+
+        //NOTE: intake user input while handling failures
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        //NOTE: converting String to num u32
+        // also parsing it to remove spaces
+        // error handling using parse Result enum
+        // Err(_) is a catch-all value
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        // NOTE: another use of match for comparison
+        // notice the use of break opening a block on one arm
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
