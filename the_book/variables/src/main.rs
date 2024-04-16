0d70dd46997e2dec65e875723986a63a89bed517
Memnoc <smartdroidesign@gmail.com>
2024-04-16 07:02:52 +0100

    varibles - mut concept

--- the_book/variables/src/main.rs
+++ the_book/variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("== Variables ==");
+
+    //NOTE: without the word mut, re-assignment is not possible
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
