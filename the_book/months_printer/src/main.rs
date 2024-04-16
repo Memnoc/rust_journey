e33596ece7e58708a245d99bd36401f59d0d7135
Memnoc <smartdroidesign@gmail.com>
2024-04-16 18:53:44 +0100

    months printer - simple program to print months strings(names) by months number(index)

---
+++ the_book/months_printer/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn main() {
+    println!("== Months Printer ==");
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    println!("== Please enter a month number ==");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = months[index - 1];
+
+    println!("The value of month number {index} is: {element}");
+}
