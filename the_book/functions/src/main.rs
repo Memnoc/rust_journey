fdf43f5ad1a51980f7655c38ed509755c23a46dd
Memnoc <smartdroidesign@gmail.com>
2024-04-17 05:48:37 +0100

    functions - declaration and invoaction

---
+++ the_book/functions/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("== Functions ==");
+
+    // NOTE: calling another function
+    another_function();
+}
+
+// NOTE: declaring another_function
+fn another_function() {
+    println!("Another function.");
+}
