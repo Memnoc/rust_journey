d77ea13b35f0f943d502075507c95bf987c548cc
Memnoc <smartdroidesign@gmail.com>
2024-04-23 11:02:16 +0100

    deleted target folder

--- the_book/ownership/target/debug/deps/ownership-588294be633417cf.d
+++
@@ -1,9 +0,0 @@
-/home/memnoc/Code/rust/the_book/ownership/target/debug/deps/libownership-588294be633417cf.rmeta: src/main.rs Cargo.toml
-
-/home/memnoc/Code/rust/the_book/ownership/target/debug/deps/ownership-588294be633417cf.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
