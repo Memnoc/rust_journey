c4bda54de772a1e2f759789aa288db97f7e62089
Memnoc <smartdroidesign@gmail.com>
2025-03-09 10:20:40 +0000

    quick stack example

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,4 +1,13 @@
 // INFO: just a way to test the concepts fromt the book
 fn main() {
     println!("=== Playground ===");
+
+    let mut x = 10; // popped off of stack
+    let y = x; // y now owns it
+    println!("{} {}", x, y); // this is fine
+
+    {
+        x = 10;
+        println!("Another x: {}", x);
+    }
 }
